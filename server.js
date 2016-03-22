458f66df93a7a3f0eb22fb890ec866b0e5b8c7f0
tyler liu <replicat0r.misc@gmail.com>
2016-03-21 22:27:32 -0400

    added log for socket connection

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var tls = require('tls')
-var fs = require ('fs')
+var fs = require('fs')
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = 5000
 var TLS_PORT = 2000
-var incomingData 
+var incomingData
 
 
 
@@ -16,32 +16,47 @@ var options = {
 }
 
 
-tls.createServer(options, function(socket){
-  console.log('TLS connection established ')
-  socket.addListener('data',function(data){
-      //console.log(data)
-      incomingData = data
-      console.log('Data Received Ok!')
-  })
-  socket.write('Listening On Port ' + TLS_PORT)
-  socket.write('You are listening on port '+ TLS_PORT + " Tom!")
-  //socket.pipe(socket)
+tls.createServer(options, function(s) {
+    console.log('TLS connection established ')
+    s.addListener('data', function(data) {
+        incomingData = data
+        console.log('Data Received Ok!')
+    })
+
+    console.log("TLS Client authorized:", s.authorized);
+    if (!s.authorized) {
+        console.log("TLS authorization error:", s.authorizationError);
+    }
+
+    console.log("Cipher: ", s.getCipher());
+    console.log("Address: ", s.address());
+    console.log("Remote address: ", s.remoteAddress);
+    console.log("Remote port: ", s.remotePort);
+
+    var fragment = '';
   
-  
-  
-  
+    socket.write('Ok')
+
+
+    //   socket.write('Listening On Port ' + TLS_PORT)
+    //   socket.write('You are listening on port '+ TLS_PORT + " Tom!")
+    //socket.pipe(socket)
+
+
+
+
 }).listen(TLS_PORT)
 
 
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
 });
 
-io.on('connection',function(socket){
-  console.log('a user conected')
+io.on('connection', function(socket) {
+    console.log('a user conected')
 })
 
-http.listen(port,function(){
+http.listen(port, function() {
     console.log('listening on' + port)
 })
