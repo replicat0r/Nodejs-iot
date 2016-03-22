d0d0c4be2b2d1267a1905272e6cf835620aadec6
tyler liu <replicat0r.misc@gmail.com>
2016-03-21 21:43:22 -0400

    adjusged dynmaic port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 5000
+var TLS_PORT = process.env.PORT || 2000
 
 
 
@@ -21,7 +22,7 @@ tls.createServer(options, function(socket){
   })
   socket.write('Listening On Port 2000')
   socket.pipe(socket)
-}).listen(2000)
+}).listen(TLS_PORT)
 
 
 
@@ -34,5 +35,5 @@ io.on('connection',function(socket){
 })
 
 http.listen(port,function(){
-    console.log('listening on 3000')
+    console.log('listening on' + port)
 })
